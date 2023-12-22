65bc9d246ef4677b51a085fdd2fe90f5cb11b353
Vi Nguyen <nguyenkhavi01032001@gmail.com>
2023-12-22 14:06:42 +0700

    build: change handler entry

--- apps/image-resizer/src/main.ts
+++ apps/image-resizer/src/main.ts
@@ -1,7 +1,7 @@
 import { extractParams } from '@s3-utils/index';
 import { S3Event } from 'aws-lambda';
 
-export const lambdaHandler = async (event: S3Event): Promise<void> => {
+export const handler = async (event: S3Event): Promise<void> => {
   const { videoFileName, triggerBucketName } = extractParams(event);
   console.log({ videoFileName, triggerBucketName });
 };
