9c2b02a8aba6f4bbdbc2e03db1b324e08864fc90
nkvi <nkvi.dev@gmail.com>
2023-12-22 10:45:03 +0700

    Initial commit

---
+++ apps/gha-cdk-lambda-monorepo-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'gha-cdk-lambda-monorepo-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/gha-cdk-lambda-monorepo-e2e',
+};
